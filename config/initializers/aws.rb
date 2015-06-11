17c73cd975e7b6050b513790e08aab1acb6c3ea6
Magdalena Gumularz <magdalena.gumularz@netguru.co>
2015-06-11 15:01:05 +0200

    add region to AWS.config, fix code style [#95135558]

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,3 @@
-AWS.config(
-  access_key_id: AppConfig.s3_access_key_id,
-  secret_access_key: AppConfig.s3_secret_access,
-)
+AWS.config(access_key_id: AppConfig.s3_access_key_id,
+           secret_access_key: AppConfig.s3_secret_access,
+           region: AppConfig.s3_region)
