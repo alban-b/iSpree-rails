7c2c0893753b791caa65c7703fde1c515e4b5e93
Magdalena Gumularz <magdalena.gumularz@netguru.co>
2015-06-23 16:02:37 +0200

    configure database cleaner [#96869350]

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,14 +4,23 @@ require "spec_helper"
 require File.expand_path("../../config/environment", __FILE__)
 require "rspec/rails"
 require "spree/testing_support/factories"
+require "database_cleaner"
 require "ffaker"
 
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.include FactoryGirl::Syntax::Methods
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
 
-  config.use_transactional_fixtures = true
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
 
-  config.infer_spec_type_from_file_location!
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
