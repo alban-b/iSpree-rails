fd9d35933b8c0b1c2f7bc55bdc1b3805b88cf8c6
Magdalena Gumularz <magdalena.gumularz@netguru.co>
2015-06-23 16:03:21 +0200

    add route for bestsellers api endpoint [#96869350]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   mount Spree::Core::Engine, at: "/"
 
+  Spree::Core::Engine.routes.draw do
+    namespace :api do
+      get "bestsellers" => "variants#bestsellers"
+    end
+  end
 end
