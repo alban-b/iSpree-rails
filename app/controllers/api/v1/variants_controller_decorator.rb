9231b7a4d1a16cd17f0a6c2c170662c6b492bb8b
Magdalena Gumularz <magdalena.gumularz@netguru.co>
2015-06-24 09:57:14 +0200

    guard style fix (line was too long) [#96869350]

--- app/controllers/api/v1/variants_controller_decorator.rb
+++ app/controllers/api/v1/variants_controller_decorator.rb
@@ -1,7 +1,8 @@
 Spree::Api::VariantsController.class_eval do
   def bestsellers
     @bestsellers = Spree::Variant.bestsellers.ransack(params[:q]).result
-    @bestsellers = @bestsellers.distinct.page(params[:page]).per(params[:per_page])
+    @bestsellers = @bestsellers.distinct.page(params[:page])
+                   .per(params[:per_page])
     respond_with(@bestsellers)
   end
 end
